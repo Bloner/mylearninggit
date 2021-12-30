cab25bc566cafd0e30cd2722fbb54a87af6d615c
BONN <nicolaschou4@gmail.com>
2021-12-31 00:24:54 +0800

    KRC6 updated

--- C/K&R/KRC6.c
+++ C/K&R/KRC6.c
@@ -1,5 +1,13 @@
+//K&R Chapter 6 Structures
 #include <stdio.h>
 
+// 6.5 self-referential structures
+// binary search tree
+struct tnode {
+	char *word;
+	int count;
+	struct tnode *left; // you can't include the structure itself but you can do with the poiter to the structure.
+	struct tnode *right;
 int main(){
 	return 0;
 }
